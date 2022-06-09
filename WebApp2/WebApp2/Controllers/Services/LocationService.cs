7924346f064ca79dd40b0cc933c98d5d2e13339c
AlexAbdullah <alexradja97@gmail.com>
2022-06-09 20:21:52 +1000

    able to return water locations

---
+++ WebApp2/WebApp2/Controllers/Services/LocationService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.IO;
+using System.Text.Json;
+namespace WebApp2.Controller.Services
+{
+    public class LocationService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<LocationService> _logger;
+        private readonly string _url;
+
+        public LocationService(HttpClient client, ILogger<LocationService> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _logger.LogCritical("Created Service");
+            _httpClient = client;
+            var section = configuration.GetSection("HttpCustomHeaders:User-agent");
+            var locationSection = configuration.GetSection("GeocodingAPI:BaseURL");
+            _logger.LogCritical(section.Key + ": " + section.Value);
+            _logger.LogCritical("BaseURL: " + section.Value);
+            _httpClient.DefaultRequestHeaders.Add(section.Key, section.Value);
+        }
+
+        public async Task<Location[]> GetNearestWaterLocation(string location)
+        {
+            //_httpClient.DefaultRequestHeaders.Add("User-Agent", "C# App");            
+            var x = await _httpClient.GetAsync($"https://nominatim.openstreetmap.org/search?format=json&q=water+near+{location}");
+            var JsonObjectString = await x.Content.ReadAsStringAsync();
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "location.json");
+            File.WriteAllText(filePath, JsonObjectString);
+            string newJsonObject = File.ReadAllText(filePath);
+
+            return JsonSerializer.Deserialize<Location[]>(newJsonObject);             
+                        
+        }
+    }
+}
